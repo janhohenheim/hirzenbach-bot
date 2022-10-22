a0a71c324858fc63c5aaab6ea2d51ec40dc8fc81
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-10-22 13:35:03 +0200

    Move pickle handling to migrate.py

--- hirzenbach-bot/migrate.py
+++ hirzenbach-bot/migrate.py
@@ -1,4 +1,41 @@
-from persistance import Data, init_database, add_sticker, subscribe_morning, subscribe_stickers, append_to_memory
+from dataclasses import dataclass, field
+from typing import Set, Dict, List
+import pickle
+from persistance import init_database, add_sticker, subscribe_morning, subscribe_stickers, append_to_memory
+
+
+@dataclass(frozen=False)
+class Data:
+    sticker_pool: Set[str] = field(default_factory=set)
+    adding_sticker: bool = False
+    sticker_subscribers: Set[int] = field(default_factory=set)
+    morning_subscribers: Set[int] = field(default_factory=set)
+    memory: Dict[int, List[str]] = field(default_factory=dict)
+
+    def init() -> None:
+        try:
+            Data.read()
+        except FileNotFoundError:
+            Data.write(Data())
+
+    def read() -> "Data":
+        with open("data.pickle", "rb") as f:
+            data = pickle.load(f)
+            _ensure_attr(data, "sticker_pool", set())
+            _ensure_attr(data, "adding_sticker", False)
+            _ensure_attr(data, "sticker_subscribers", set())
+            _ensure_attr(data, "morning_subscribers", set())
+            _ensure_attr(data, "memory", dict())
+            return data
+
+    def write(self) -> None:
+        with open("data.pickle", "wb") as f:
+            pickle.dump(self, f)
+
+
+def _ensure_attr(obj, attr, default):
+    if not hasattr(obj, attr):
+        setattr(obj, attr, default)
 
 
 def migrate():
--- hirzenbach-bot/persistance.py
+++ hirzenbach-bot/persistance.py
@@ -1,6 +1,5 @@
-from typing import Set, Dict, List, Optional
-from dataclasses import dataclass, field
-import pickle
+from typing import List, Optional
+
 import sqlite3
 
 _SQLITE_DATABASE = 'hirzenbach_bot.sqlite3'
@@ -102,37 +101,3 @@ def is_adding_stickers(chat_id: int) -> bool:
 
 def _connect() -> sqlite3.Connection:
     return sqlite3.connect(_SQLITE_DATABASE)
-
-
-@dataclass(frozen=False)
-class Data:
-    sticker_pool: Set[str] = field(default_factory=set)
-    adding_sticker: bool = False
-    sticker_subscribers: Set[int] = field(default_factory=set)
-    morning_subscribers: Set[int] = field(default_factory=set)
-    memory: Dict[int, List[str]] = field(default_factory=dict)
-
-    def init() -> None:
-        try:
-            Data.read()
-        except FileNotFoundError:
-            Data.write(Data())
-
-    def read() -> "Data":
-        with open("data.pickle", "rb") as f:
-            data = pickle.load(f)
-            _ensure_attr(data, "sticker_pool", set())
-            _ensure_attr(data, "adding_sticker", False)
-            _ensure_attr(data, "sticker_subscribers", set())
-            _ensure_attr(data, "morning_subscribers", set())
-            _ensure_attr(data, "memory", dict())
-            return data
-
-    def write(self) -> None:
-        with open("data.pickle", "wb") as f:
-            pickle.dump(self, f)
-
-
-def _ensure_attr(obj, attr, default):
-    if not hasattr(obj, attr):
-        setattr(obj, attr, default)
